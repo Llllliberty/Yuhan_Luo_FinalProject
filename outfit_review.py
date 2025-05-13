aa78946cce90e755f6bb8f971d6459c9bae496e5
Llllliberty <2093438728@qq.com>
2025-05-13 13:43:12 +1000

    outfit_review.py

--- outfit_review.py
+++ outfit_review.py
@@ -6,4 +6,13 @@ def review_outfit(worn_clothes, weather):
     elif weather == 'cloudy' and 'sweater' in worn_clothes:
         return "😊 Your outfit was suitable for the cloudy weather!"
     else:
-        return "😢 The outfit might not have been suitable for the weather."
+        # If the outfit doesn't match the weather, add a suggestion
+        suggestion = ""
+        if weather == 'sunny':
+            suggestion = "Next time, try wearing something light like a T-shirt and shorts."
+        elif weather == 'rainy':
+            suggestion = "Next time, make sure to wear a raincoat and waterproof shoes."
+        elif weather == 'cloudy':
+            suggestion = "Next time, a sweater or light jacket would be perfect for cloudy weather."
+
+        return f"😢 The outfit might not have been suitable for the weather. {suggestion}"
